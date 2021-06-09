12e16b9be0e64988202439900e64119752ab8876
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2021-06-09 09:25:05 -0600

    Update clean_natural_earth.py

--- clean_natural_earth.py
+++ clean_natural_earth.py
@@ -20,7 +20,84 @@ def call(cmd):
 def setup_db(db):
     call('dropdb %s' % db)
     call('createdb -T template_postgis %s' % db)
-    call('wget http://www.sogis1.so.ch/sogis/dl/postgis/cleanGeometry.sql')
+    f = open("cleanGeometry.sql", "a")
+    f.write("""-- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+--  
+-- $Id: cleanGeometry.sql 2008-04-24 10:30Z Dr. Horst Duester $
+--
+-- cleanGeometry - remove self- and ring-selfintersections from  
+--                 input Polygon geometries  
+-- http://www.sogis.ch
+-- Copyright 2008 SO!GIS Koordination, Kanton Solothurn, Switzerland
+-- Version 1.0
+-- contact: horst dot duester at bd dot so dot ch
+--
+-- This is free software; you can redistribute and/or modify it under
+-- the terms of the GNU General Public Licence. See the COPYING file.
+-- This software is without any warrenty and you use it at your own risk
+--   
+-- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+
+CREATE OR REPLACE FUNCTION cleanGeometry(geometry)
+   RETURNS geometry AS
+$BODY$DECLARE
+   inGeom ALIAS for $1;
+   outGeom geometry;
+   tmpLinestring geometry;
+   tmpPolygon geometry;
+   Pk geometry;
+   Pi geometry;
+   nGeometries integer;
+   nGeom integer;
+   intRingPi geometry;
+   nIntRings integer;
+   extRingPk geometry;
+   isHole boolean;
+
+Begin
+
+   outGeom := NULL;
+
+-- Clean Process for Polygon  
+   IF (GeometryType(inGeom) = 'POLYGON' OR GeometryType(inGeom) =  
+'MULTIPOLYGON') THEN
+
+-- Only process if geometry is not valid, 
+-- otherwise put out without change
+     if not isValid(inGeom) THEN
+
+-- create nodes at all self-intersecting lines by union the polygon  
+boundaries
+-- with the startingpoint of the boundary.  
+       tmpLinestring :=  
+st_union(st_multi(st_boundary(inGeom)),st_pointn(boundary(inGeom),1));
+       outGeom = buildarea(tmpLinestring);
+       RETURN st_multi(outGeom);
+     else
+       RETURN st_multi(inGeom);
+     END IF;
+
+
+------------------------------------------------------------------------------
+-- Clean Process for LINESTRINGS, self-intersecting parts of linestrings 
+-- will be divided into multiparts of the mentioned linestring 
+------------------------------------------------------------------------------
+   ELSIF (GeometryType(inGeom) = 'LINESTRING' OR GeometryType(inGeom)  
+= 'MULTILINESTRING') THEN
+
+-- create nodes at all self-intersecting lines by union the linestrings
+-- with the startingpoint of the linestring.  
+     outGeom := st_union(st_multi(inGeom),st_pointn(inGeom,1));
+     RETURN outGeom;
+   ELSE
+     RAISE EXCEPTION 'The input type % is not  
+supported',GeometryType(inGeom);
+   END IF;	
+End;$BODY$
+   LANGUAGE 'plpgsql' VOLATILE;
+""")
+    f.close()
     call('psql %s -f cleanGeometry.sql' % db)
 
 def fix_ne(url,db,clipping_extent):
